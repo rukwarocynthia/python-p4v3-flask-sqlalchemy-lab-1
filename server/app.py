5df6db78629d92298eec0c10c1119d94f0c1250e
rukwarocynthia4093@gmail.com <rukwarocynthia4093@gmail.com>
2024-06-30 20:33:47 +0300

    done

--- server/app.py
+++ server/app.py
@@ -20,6 +20,26 @@ def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
+@app.route('/earthquakes/<int:id>')
+def get_earthquake_by_id(id):
+    earthquake = db.session.get(Earthquake, id)
+    if not earthquake: 
+        # return {f"message": "Earthquake {id} not found."}, 404
+        return {"message": f"Earthquake {id} not found."}, 404
+    return(earthquake.to_dict()), 200
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_earthquake_by_mangnitude(magnitude):
+    earthquakes = []
+    for earthquake in Earthquake.query.filter(Earthquake.magnitude >= magnitude).all():
+        earthquakes.append(earthquake.to_dict())
+    body = {
+        'count' : len(earthquakes),
+        'quakes' : earthquakes
+    }
+
+    return body, 200
+
 # Add views here
 
 
